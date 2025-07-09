6245802d924f0fe2bf838ccdfa194233381198b9
yasuyaswa <83386305+yasuyaswa@users.noreply.github.com>
2025-07-09 21:41:25 +0530

    Create RemoveDuplicatesInArray.cs

---
+++ CSharp/Integer/RemoveDuplicatesInArray.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C__Practice.Integer
+{
+    public class RemoveDuplicatesInArray
+    {
+        public static void RemoveDuplicates(){
+            int[] a= {6,3,7,2,9,6,2,7,4,8};
+            int[] SortArr = a.Distinct().OrderBy(o=>o).ToArray();
+            string result = string.Join(",", SortArr);
+            Console.WriteLine(result);
+        }
+    }
+}
